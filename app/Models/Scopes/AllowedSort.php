89ea0d33f30c2743d678f2b9c2366820ea9349be
asaita <saitayyildiz@gmail.com>
2023-12-25 14:10:25 +0300

    lecture 118

--- app/Models/Scopes/AllowedSort.php
+++ app/Models/Scopes/AllowedSort.php
@@ -7,24 +7,29 @@ use Illuminate\Database\Eloquent\Builder;
 trait AllowedSort
 {
 
-    public function parseSortDirection(){
+    public function parseSortDirection($column=null){
 
-        return strpos(request()->query('sort_by'),"-")===0 ? 'desc' : 'asc';
+        return strpos($column ?? request()->query('sort_by'),"-")===0 ? 'desc' : 'asc';
         
     }
 
-    public function parseSortColumn() {
-        return ltrim(request()->query('sort_by'),'-');
+    public function parseSortColumn($column=null) {
+        return ltrim($column ?? request()->query('sort_by'),'-');
         
     }
 
-    public function scopeAllowedSorts(Builder $query, array $columns)
+    public function scopeAllowedSorts(Builder $query, array $columns, $defaultColumn=null)
     {
         $column=$this->parseSortColumn();
 
         if(in_array($column, $columns)){
 
             return $query->orderBy($column,$this->parseSortDirection());
+        }
+
+        if(!$column&&$defaultColumn){
+
+            return $query->orderBy($this->parseSortColumn($defaultColumn),$this->parseSortDirection($defaultColumn));
         }
 
         return $query;

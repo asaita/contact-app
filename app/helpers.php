44304a0436bae60aeeb8ff21cf2f18cd891483ea
asaita <saitayyildiz@gmail.com>
2023-12-27 14:34:48 +0300

    sortable style

--- app/helpers.php
+++ app/helpers.php
@@ -5,12 +5,22 @@
 function sortable($label, $column){
 
     $sortBy=request()->query('sort_by');
+
+    $direction="";
+
+
+    if(ltrim($sortBy,'-')===$column){
+
+        $direction=strpos($sortBy,'-')===0 ? "desc" : "asc";
+    }
     
     //iki kere basınca - koyuyuor url nin önüne
     $sortBy=!$sortBy||strpos($sortBy,"-")===0 ? $column : "-{$column}";
     $url=request()->fullUrlWithQuery(['sort_by'=>$sortBy]);
 
-    return "<a href='{$url}'>{$label}</a>";
+    
+
+    return "<a href='{$url}' class='sortable {$direction}'>{$label}</a>";
 
 }
 
